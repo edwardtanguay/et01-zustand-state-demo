7527dedb33b24918dd01803d5694fa988e12244d
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 06:59:06 +0200

    one component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+	const [message, setMessage] = useState('');
+
 	return (
 		<div className="App">
 			<h1>Zustand Demo</h1>
-			<p>testing</p>
+			<main>
+				<section className="controlArea">
+					<div className="data">
+						<label>Message:</label>
+						<input
+							autoFocus
+							type="text"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+						TEST: {message}
+					</div>
+				</section>
+			</main>
 		</div>
 	);
 }
